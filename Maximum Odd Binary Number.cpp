6976a0e3300b2ce3d4b4460fe596c4efc1fb4900
123456-manvi <103314650+123456-manvi@users.noreply.github.com>
2024-03-01 23:40:59 +0530

    Update Maximum Odd Binary Number.cpp

--- Maximum Odd Binary Number.cpp
+++ Maximum Odd Binary Number.cpp
@@ -42,5 +42,38 @@ public:
 
         res[n-1]='1';
         return res;
+
+
+
+        
+        // 3 way........................
+        // sort(s.rbegin(),s.rend());
+        // for(int i=s.size()-1;i>=0;i--)
+        // {
+        //     if(s[i]=='1')
+        //     {
+        //         swap(s[i],s[s.size()-1]);
+        //     }
+            
+        // }
+        // return s;
+
+
+
+    //   4 way....................
+    int count_1=0;
+    int count_0=0;
+    for(int i=0;i<s.length();i++)
+    {
+        if(s[i]=='1')
+        count_1++;
+        if(s[i]=='0')
+        count_0++;
+    }
+
+    string ans;
+    ans=string(count_1-1,'1')+string(count_0,'0')+'1';
+     return ans;
+
     }
 };
