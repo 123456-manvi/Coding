5b34a6332a451a3987a421c6b208dced2353d99e
123456-manvi <103314650+123456-manvi@users.noreply.github.com>
2024-03-03 15:33:50 +0530

    Create Largest Number formed from an Array.cpp

---
+++ Largest Number formed from an Array.cpp
@@ -0,0 +1,21 @@
+class Solution{
+public:
+	// The main function that returns the arrangement with the largest value as
+	// string.
+	// The function accepts a vector of strings
+	
+	static bool compare(const string &a, const string &b)
+	{
+	    return (a+b) > (b+a);
+	}
+	string printLargest(int n, vector<string> &arr) {
+	    // code here
+	    sort(arr.begin(),arr.end(), compare);
+	    string res;
+	    for(int i=0;i<n;i++)
+	    {
+	        res+=arr[i];
+	    }
+	    return res;
+	}
+};
