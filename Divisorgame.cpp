5a561fd70035956c70a79498e9c6ec4b9f9beb70
123456-manvi <103314650+123456-manvi@users.noreply.github.com>
2024-05-09 19:03:44 +0530

    Create DivisorGame.cpp

---
+++ Divisorgame.cpp
@@ -0,0 +1,7 @@
+// Choosing any x with 0 < x < n  and n % x == 0.
+// Replacing the number n on the chalkboard with n - x.
+
+ bool divisorGame(int n) {
+        // code here
+        return n%2==0;
+    }
