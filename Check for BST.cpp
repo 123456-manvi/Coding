26999e748b405d1618d228ea75d96aef182bef61
123456-manvi <103314650+123456-manvi@users.noreply.github.com>
2024-07-24 14:57:27 +0530

    Create Check for BST.cpp

---
+++ Check for BST.cpp
@@ -0,0 +1,25 @@
+class Solution{
+ public:
+    // Function to check whether a Binary Tree is BST or not
+    // use inorder to traverse (root->left,data,root->right)
+    void inorder(Node* root, vector<int> &ans){
+        if(root==NULL){
+            return;
+        }
+        inorder(root->left,ans);
+        ans.push_back(root->data);
+        inorder(root->right,ans);
+    }
+    bool isBST(Node* root) {
+        // Your code here
+        vector<int>ans;
+        inorder(root,ans);
+        for(int i=0;i<ans.size()-1;i++){
+            if(ans[i]>=ans[i+1]){
+                return false;
+            }
+        }
+        
+      return true;
+    }
+};
